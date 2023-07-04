4acc9a289d2d0aeaf1ded4c7a9eefd2b94b61a5c
DeogHoon Hwang <harry.d.hwang@DeogHoons-MacBook-Air.local>
2023-07-05 00:21:43 +0900

    4.2 AuthButton

--- lib/features/widgets/auth_button.dart
+++ lib/features/widgets/auth_button.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 
 class AuthButton extends StatelessWidget {
   final String text;
   // text 변수를 선언해줌
+  final FaIcon icon;
 
   const AuthButton({
     super.key,
     required this.text,
+    required this.icon,
   });
   // super.key 커서 이동 후 code action 열어서 final field parameter initialize 버튼 클릭
 
@@ -17,20 +20,34 @@ class AuthButton extends StatelessWidget {
       // FractionallySizedBox : 절대값을 갖지 않되 father 크기에 상대적인 fraction 비율 갖는 박스
       widthFactor: 1,
       child: Container(
-        padding: const EdgeInsets.symmetric(vertical: Sizes.size14),
+        padding: const EdgeInsets.all(
+          Sizes.size14,
+        ),
         decoration: BoxDecoration(
           border: Border.all(
             color: Colors.grey.shade300,
             width: Sizes.size1,
           ),
         ),
-        child: Text(
-          text,
-          textAlign: TextAlign.center,
-          style: const TextStyle(
-            fontSize: Sizes.size16,
-            fontWeight: FontWeight.w600,
-          ),
+        child: Stack(
+          // Row 대신 Stack을 사용.
+          //  A stack allows you to put items on top of each other. 레이어 겹치기
+          alignment: Alignment.center,
+          children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: icon,
+              // icon 을 align으로 감싸주면 하나의 class에 대해서만 배열 효과를 줄 수 있다.
+            ),
+            Text(
+              text,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                fontSize: Sizes.size16,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ],
         ),
       ),
     );
